9f9f9a2f5f7f6deb2044ddbd0dfe8adf8eb1cce3
mendozadavid0322 <77967483+mendozadavid0322@users.noreply.github.com>
2021-01-26 12:18:35 +0800

    Update and rename Imminent Sunrise.html to index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head> <title> Imminent Sunrise </title> </head>
 <body>
 <Body Background= "https://i.pinimg.com/originals/fd/eb/a4/fdeba43a9d969e0a5819f73e5a1d74bf.jpg">
-<p align= "center"> <img src= "Pictures/Imminent Sunrise Logo.png"> </p>
+<p align= "center"> <img src= "Pictures/Imminent Sunrise Logo.png.png"> </p>
 <br> <Font face= Verdana, color="black", size= "5">  The Threat of Human Health </p> 
 <p align= "right"> The Ignition of Hope </font> </p>
 <br> <br> <br> <p align= "center"> <Font face= Arial, color= "yellow", size= "5"> <b> <i> Welcome to Imminent Sunrise! <br> 
